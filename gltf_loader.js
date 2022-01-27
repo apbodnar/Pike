94cd896f4c868de12bc112e7dc90e4c6826f4abd
Adam Bodnar <adam.p.bodnar@gmail.com>
2022-01-27 00:26:49 -0800

    git messup

---
+++ gltf_loader.js
@@ -0,0 +1,294 @@
+import * as utils from './utility.js'
+import { Vec3 } from './vector.js';
+
+const INT16 = 5122;
+const UINT16 = 5123;
+const UINT32 = 5125;
+const FLOAT32 = 5126;
+
+const STRIDE = {
+  5122: 2,
+  5123: 2,
+  5125: 4,
+  5126: 4,
+};
+
+const TRIANGLES = 4;
+/**
+ * A small, mostly useless GLTF loader.
+ */
+const ELEMENT_COUNT = {
+  SCALAR: 1,
+  VEC2: 2,
+  VEC3: 3,
+  VEC4: 4.
+}
+
+export class GLTFLoader {
+  constructor() {
+    this.manifest = null;
+    this.buffers = null;
+    this.bufferViews = null;
+    this.accessors = null;
+    this.images = null;
+    this.meshes = [];
+  }
+
+  async load(url) {
+    const split = url.split('/');
+    split.pop();
+    const pwd = split.join('/') + '/';
+    this.manifest = await (await fetch(url)).json();
+    // Only support GLTF 2.0
+    console.assert(this.manifest.asset.version === "2.0", "Wrong GLTF version");
+    // Fetch the blobs
+    this.buffers = await Promise.all(
+      this.manifest.buffers.map(async (b) => { return (await fetch(pwd + b.uri)).arrayBuffer(); })
+    );
+    // Init buffer views;
+    this.bufferViews = this.manifest.bufferViews.map((v) => {
+      const buffer = this.buffers[v.buffer];
+      return new DataView(buffer, v.byteOffset, v.byteLength);
+    });
+    // Fetch the images
+    this.images = this.manifest.images ? await Promise.all(
+      this.manifest.images.map((i) => { return utils.getImage(pwd + i.uri); })
+    ) : [];
+    const scene = this.manifest.scenes[this.manifest.scene];
+    for (const nodeIdx of scene.nodes) {
+      const root = this.nodeDescAt(nodeIdx);
+      this.constructMeshes(root);
+    }
+    return new GLTF(this.meshes, this.manifest.asset);
+  }
+
+  accessorDescAt(i) {
+    return this.manifest.accessors[i];
+  }
+
+  nodeDescAt(i) {
+    return this.manifest.nodes[i];
+  }
+
+  meshDescAt(i) {
+    return this.manifest.meshes[i];
+  }
+
+  bufferViewDescAt(i) {
+    return this.manifest.bufferViews[i];
+  }
+
+  materialDescAt(i) {
+    return this.manifest.materials[i];
+  }
+
+  constructMeshes(node) {
+    if ('mesh' in node) {
+      const meshDesc = this.meshDescAt(node.mesh);
+      this.meshes.push(new GLTFMesh(meshDesc, null, this))
+    }
+    if (node.children) {
+      for (const childIdx of node.children) {
+        const child = this.nodeDescAt(childIdx);
+        this.constructMeshes(child);
+      }
+    }
+  }
+
+  getMeshes() {
+    return this.meshes;
+  }
+}
+
+class GLTFMaterial {
+  constructor(desc, loader) {
+    this.id = Math.round(Math.random() * 10000);
+    this.desc = desc;
+    this.loader = loader;
+  }
+
+  getBaseColor() {
+    return this.desc.pbrMetallicRoughness?.baseColorFactor?.slice(0, 3) ?? [0.8, 0.8, 0.8];
+  }
+
+  getMetallicRoughness() {
+    return [0, this.desc.pbrMetallicRoughness?.roughnessFactor ?? 0.3, this.desc.pbrMetallicRoughness?.metallicFactor ?? 0];
+  }
+
+  hasNormalTexture() {
+    return !!this.desc.normalTexture;
+  }
+
+  getNormalTexture() {
+    return this.loader.images[this.desc.normalTexture.index];
+  }
+
+  hasMetallicRoughnessTexture() {
+    return !!this.desc.pbrMetallicRoughness?.metallicRoughnessTexture;
+  }
+
+  getMetallicRoughnessTexture() {
+    const idx = this.desc.pbrMetallicRoughness.metallicRoughnessTexture.index;
+    return this.loader.images[idx];
+  }
+
+  hasBaseColorTexture() {
+    return !!this.desc.pbrMetallicRoughness?.baseColorTexture;
+  }
+
+  getBaseColorTexture() {
+    const idx = this.desc.pbrMetallicRoughness.baseColorTexture.index;
+    return this.loader.images[idx];
+  }
+}
+
+class GLTFPrimitive {
+  constructor(desc, mesh) {
+    this.desc = desc;
+    this.mesh = mesh;
+    this.loader = this.mesh.loader;
+    this.material = new GLTFMaterial(this.loader.materialDescAt(this.desc.material), this.loader);
+    if (this.desc.attributes.NORMAL === undefined) {
+      this.computedNormals = new Array(this.indexCount()).fill({ n: [0, 0, 0], count: 0 });
+      this._computeNormals();
+    }
+    console.assert(this.desc.mode === TRIANGLES);
+  }
+
+  indexCount() {
+    const accessorIdx = this.desc.indices;
+    const accessorDesc = this.loader.accessorDescAt(accessorIdx);
+    return accessorDesc.count;
+  }
+
+  indexAt(i) {
+    const accessorIdx = this.desc.indices;
+    return this._getAccessorValueAt(accessorIdx, i);
+  }
+
+  attributeCount() {
+    const accessorIdx = this.desc.attributes.POSITION;
+    const accessorDesc = this.loader.accessorDescAt(accessorIdx);
+    return accessorDesc.count;
+  }
+
+  positionAt(i) {
+    const accessorIdx = this.desc.attributes.POSITION;
+    return this._getAccessorValueAt(accessorIdx, i);
+  }
+
+  uvAt(i) {
+    const accessorIdx = this.desc.attributes.TEXCOORD_0;
+    if (accessorIdx !== undefined) {
+      return this._getAccessorValueAt(accessorIdx, i);
+    } else {
+      return [0.5, 0.5];
+    }
+  }
+
+  normalAt(i) {
+    const accessorIdx = this.desc.attributes.NORMAL;
+    if (accessorIdx !== undefined) {
+      return this._getAccessorValueAt(accessorIdx, i);
+    } else {
+      return Vec3.normalize(this.computedNormals[i].n);
+    }
+
+  }
+
+  tangentAt(i) {
+    const accessorIdx = this.desc.attributes.TANGENT;
+    if (accessorIdx !== undefined) {
+      return this._getAccessorValueAt(accessorIdx, i);
+    } else {
+      const normal = this.normalAt(i);
+      return Math.abs(normal[2]) < 0.999 ? Vec3.normalize(Vec3.cross(normal, [0, 1, 0])) : [1, 0, 0];
+    }
+  }
+
+  _addComputedNormal(normal, i) {
+    const current = Vec3.scale(this.computedNormals[i].n, count);
+    this.computedNormals[i].count++;
+    const n = Vec3.scale(Vec3.sum(current, normal), 1 / this.computedNormals[i].count);
+    this.computedNormals[i].n = n;
+  }
+
+  _computeNormals() {
+    for (let i = 0; i < this.indexCount(); i += 3) {
+      const p0 = positionAt(i);
+      const p1 = positionAt(i + 1);
+      const p2 = positionAt(i + 2);
+      const e0 = Vec3.sub(p1, p0);
+      const e1 = Vec3.sub(p2, p0);
+      const n = Vec3.cross(e0, e1);
+      this._addComputedNormal(n, i);
+      this._addComputedNormal(n, i + 1);
+      this._addComputedNormal(n, i + 2);
+    }
+  }
+
+  _getAccessorValueAt(accessorIdx, i) {
+    const accessorDesc = this.loader.accessorDescAt(accessorIdx);
+    const offsetBytes = accessorDesc.byteOffset ?? 0;
+    const bufferViewDesc = this.loader.accessorDescAt(accessorDesc.bufferView);
+    const stride = bufferViewDesc.byteStride ?? this._getStride(accessorDesc);
+    const view = this.loader.bufferViews[accessorDesc.bufferView];
+    const start = i * stride + offsetBytes;
+    return this._getTypedValue(view, start, accessorDesc.componentType, accessorDesc.type);
+  }
+
+  _getStride(accessorDesc) {
+    return STRIDE[accessorDesc.componentType] * ELEMENT_COUNT[accessorDesc.type];
+  }
+
+  _getTypedValue(view, start, compType, elementType) {
+    const elementCount = ELEMENT_COUNT[elementType];
+    const vec = new Array(elementCount);
+    for (let i = 0; i < elementCount; i++) {
+      switch (compType) {
+        case INT16: {
+          vec[i] = view.getInt16(start + i * 2, true);
+          break;
+        }
+        case UINT16: {
+          vec[i] = view.getUint16(start + i * 2, true);
+          break;
+        }
+        case UINT32: {
+          vec[i] = view.getUint32(start + i * 4, true);
+          break;
+        }
+        case FLOAT32: {
+          vec[i] = view.getFloat32(start + i * 4, true);
+          break;
+        }
+        default: {
+          throw new Error("Unsupported element type");
+        }
+      }
+    }
+    return vec;
+  }
+}
+
+class GLTFMesh {
+  constructor(desc, transform, loader) {
+    this.desc = desc;
+    this.transform = transform;
+    this.loader = loader;
+    this.primitives = this.desc.primitives.map((p) => {
+      return new GLTFPrimitive(p, this);
+    });
+  }
+
+  getPrimitives() {
+    return this.primitives;
+  }
+}
+
+export class GLTF {
+  constructor(meshes, asset) {
+    this.meshes = meshes;
+    this.asset = asset;
+  }
+}
---
+++ scene.js
@@ -0,0 +1,149 @@
+import { GLTFLoader } from "./gltf_loader.js";
+import { Vec3 } from './vector.js'
+import * as utils from './utility.js'
+import { TexturePacker } from "./texture_packer.js";
+
+export class Scene {
+  constructor() {
+    this.desc = null;
+    this.groups = null;
+    this.materials = [];
+    this.materialIds = {};
+    this.texturePacker = new TexturePacker(2048);
+  }
+  async load(uri) {
+    this.desc = await (await fetch(uri)).json();
+    this.groups = await Promise.all(this.desc.props.map((prop) => {
+      return new GLTFLoader().load(prop.uri);
+    }))
+    this.groups.forEach((g, i) => {
+      g.transforms = this.desc.props[i].transforms;
+      g.skipMaterials = new Set(this.desc.props[i].skipMaterials);
+      g.skipMeshes = new Set(this.desc.props[i].skipMeshes);
+    });
+    [this.indexCount, this.attributeCount] = this._counts();
+    this.attributes = [];
+    this.indices = [];
+    this._createBuffers();
+    this.env = await utils.getImage(this.desc.environment);
+    return this;
+  }
+
+  getTextureAtlas() {
+
+  }
+
+  _createSceneMaterial(material) {
+    const diffMap = material.hasBaseColorTexture() ?
+      this.texturePacker.addTexture(material.getBaseColorTexture(), true) :
+      this.texturePacker.addColor(material.getBaseColor());
+    const metRoughMap = material.hasMetallicRoughnessTexture() ?
+      this.texturePacker.addTexture(material.getMetallicRoughnessTexture()) :
+      this.texturePacker.addColor(material.getMetallicRoughness());
+    const normMap = material.hasNormalTexture() ?
+      this.texturePacker.addTexture(material.getNormalTexture()) :
+      this.texturePacker.addColor([0.5, 0.5, 1]);
+    return { diffMap, metRoughMap, normMap, emitMap: 0 };
+  }
+
+  _applyGroupTransform(group,) {
+
+  }
+
+  _applyRotations(vert, transforms) {
+    transforms.rotate.forEach((r) => { vert = Vec3.rotateArbitrary(vert, r.axis, r.angle) });
+    return vert;
+  }
+
+  _applyVectorTransforms(vert, transforms, rotationOnly = false) {
+    let modelTransformed = Vec3.add(Vec3.scale(this._applyRotations(vert, transforms), rotationOnly ? 1 : transforms.scale), rotationOnly ? [0, 0, 0] : transforms.translate);
+    return modelTransformed;
+  }
+
+  _counts() {
+    let indexCount = 0;
+    let attributeCount = 0;
+    for (const group of this.groups) {
+      for (const mesh of group.meshes) {
+        for (const primitive of mesh.primitives) {
+          attributeCount += primitive.attributeCount();
+          indexCount += primitive.indexCount()
+        }
+      }
+    }
+    return [indexCount, attributeCount];
+  }
+
+  _createBuffers() {
+    let offset = 0;
+    for (const group of this.groups) {
+      for (const mesh of group.meshes) {
+        if (group.skipMeshes.has(mesh.desc.name)) {
+          continue;
+        }
+        for (const primitive of mesh.primitives) {
+          if (primitive.material?.desc?.name) {
+            if (group.skipMaterials.has(primitive.material.desc.name)) {
+              continue;
+            }
+          }
+          let matId;
+          if (this.materialIds[primitive.material.id] !== undefined) {
+            matId = this.materialIds[primitive.material.id];
+          } else {
+            matId = this.materials.length;
+            this.materialIds[primitive.material.id] = matId;
+            this.materials.push(this._createSceneMaterial(primitive.material));
+          }
+          const indexCount = primitive.indexCount();
+          for (let i = 0; i < indexCount; i += 3) {
+            this.indices.push({
+              i0: offset + primitive.indexAt(i)[0],
+              i1: offset + primitive.indexAt(i + 1)[0],
+              i2: offset + primitive.indexAt(i + 2)[0],
+              matId,
+            });
+          }
+          const attrCount = primitive.attributeCount();
+          offset += attrCount;
+          for (let i = 0; i < attrCount; i++) {
+            const normal = this._applyVectorTransforms(primitive.normalAt(i), group.transforms, true);
+            const tangent = this._applyVectorTransforms(primitive.tangentAt(i).slice(0, 3), group.transforms, true);
+            const bitangent = Vec3.normalize(Vec3.cross(primitive.normalAt(i), primitive.tangentAt(i)));
+            this.attributes.push({
+              pos: this._applyVectorTransforms(primitive.positionAt(i), group.transforms),
+              tangent: Vec3.normalize(tangent),
+              normal: Vec3.normalize(normal),
+              uv: primitive.uvAt(i),
+              bitangent: bitangent,
+            });
+          }
+        }
+      }
+    }
+  }
+
+  dumpGroup(i) {
+    for (const mesh of this.groups[i].meshes) {
+      for (const primitive of mesh.primitives) {
+        console.log("indices:", primitive.indexCount(), "attributes:", primitive.attributeCount());
+      }
+    }
+  }
+}
+
+export class SceneGeometry {
+  constructor() {
+
+  }
+
+  get indices() {
+
+  }
+
+  get attributes() {
+
+  }
+
+
+}
